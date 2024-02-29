7f4bc78892c5723c18bd233ae01bea051a4e07e8
Jakob Strüßmann <jakob.struessmann@titanom.com>
2024-02-29 11:03:32 +0100

    ommit

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,13 +2,26 @@ import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
 export default function Page() {
   return (
-    <div>
+    <div className="bg-white">
       <div>
         <div className="bg-[#91a3b0] py-5 flex items-center justify-between w-full font-bold text-2xl px-4">
           <ArrowBackIosIcon />
           <div className="mx-auto">Einstellungen</div>
-        </div>{" "}
+        </div>
         <div className="bg-black h-1 w-full" />
+        <div>
+          <div className=" px-2 text-xl text-bold bg-gray-400 py-1 flex w-full tracking-widest justify-start font-gray-200">
+            Berechtigungen
+          </div>
+          <div className="px-2">
+            <div className="font-semibold">Mitteilung</div>
+            <div className="text-gray-500">
+              CigSnacks sendet Ihnen Mitteilungen über neue Automaten in Ihrer
+              Nähe sowie Neuerungen auf der Website.
+            </div>
+            <div className="h-1 bg-black w-3/4" />
+          </div>
+        </div>
       </div>
     </div>
   );
