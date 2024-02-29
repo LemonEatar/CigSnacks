7816ec0178ff680cab4e7af034d6adf89090d289
Jakob Strüßmann <jakob.struessmann@titanom.com>
2024-02-29 10:37:32 +0100

    feat: base settings page

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <div>
+      <div>
+        <div className="bg-[#91a3b0] py-5 flex items-center justify-center w-full font-bold text-2xl">
+          Einstellungen
+        </div>
+        <div className="bg-black h-1 w-full" />
+      </div>
+    </div>
+  );
+}
